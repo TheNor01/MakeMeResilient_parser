ced6129425f5b4c58f518c3077f1ef451c57c087
Aldo Fiorito <42089908+TheNor01@users.noreply.github.com>
2022-09-13 18:18:43 +0200

    Refactor parser

--- src/main/java/ContractDiscovery.java
+++ src/main/java/ContractDiscovery.java
@@ -18,9 +18,7 @@ import com.github.javaparser.utils.SourceRoot;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -30,12 +28,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.github.javaparser.StaticJavaParser.parseStatement;
-//TESTARE CLASSI E CORRELAZIONE LINEE
 
 public class ContractDiscovery {
 
     private static String path;
-
     private static TypeSolver typeSolver = null;
     private static final TypeSolver reflection = new ReflectionTypeSolver();
     private static final TypeSolver primitiveSolver = new MemoryTypeSolver();
@@ -44,24 +40,7 @@ public class ContractDiscovery {
 
     public static void main(String[] args) throws Exception {
 
-        //File selector PROJECT
-        JFileChooser f = new JFileChooser();
-        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        f.setDialogTitle("Open project FOLDER");
-        f.showSaveDialog(null);
-
-        path = f.getCurrentDirectory().toString();
-        System.out.println("SELECTED:"+path);
-
-
-        String lastPath = path.substring(path.lastIndexOf('/') + 1);
-        if(!lastPath.equals("java")) {
-            System.out.println("project path is wrong, make sure that your last folder is /java");
-            System.exit(1);
-        }
-
-        //controllo sul path
-        typeSolver = new JavaParserTypeSolver(new File(path));
+        String pomPath = SelectProjectsPath();
 
         Path ROOT_PATH = Paths.get(path);
         JavaSymbolSolver symbolSolver = SetupSimbolSolver();
@@ -75,18 +54,6 @@ public class ContractDiscovery {
         }
 
         System.out.println(path);
-
-        JFileChooser f2 = new JFileChooser();
-        FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter(
-                "xml files (*.xml)", "xml");
-        f2.setDialogTitle("Open pom file");
-        f2.setFileFilter(xmlfilter);
-        f2.showOpenDialog(null);
-        f2.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
-        String pomPath = f2.getSelectedFile().getAbsolutePath();
-        System.out.println(pomPath);
-
         PomInject pomInjector = new PomInject(pomPath);
         pomInjector.addFailSafeDep();
         pomInjector.printAllDeps();
@@ -94,53 +61,44 @@ public class ContractDiscovery {
 
         final List<String> interfaceToLookup = new ArrayList<String>();
         LookupForRemoteInterface(all_cu, interfaceToLookup);
-
         System.out.println("FOUND REMOTE INTERFACE " + interfaceToLookup.stream().findFirst().toString());
 
         all_cu.forEach(cu->{
-
             reWriteClass = 0;
             List<Integer> lines = new ArrayList<Integer>();
             Optional<CompilationUnit> localCu =  cu.getResult();
 
             if(!localCu.isPresent()) return;
-            localCu.get().addImport(new ImportDeclaration("dev.failsafe.Failsafe", false, false));
-            localCu.get().addImport(new ImportDeclaration("dev.failsafe.RetryPolicy", false, false));
-            localCu.get().addImport(new ImportDeclaration("java.time.temporal.ChronoUnit", false, false));
+            AddImportDeclaration(localCu);
 
             List<Expression> exps = localCu.get().findAll(Expression.class).stream()
                     .filter(ae -> ae.isCastExpr() || ae.isAssignExpr())
                     .collect(Collectors.toList());
 
             HashMap<Expression,Boolean> validExp = new HashMap<Expression,Boolean>();
-            for(Expression exp: exps){
-                validExp.put(exp,Boolean.TRUE);
-            }
+            for(Expression exp: exps) validExp.put(exp,Boolean.TRUE);
+
             for(Expression exp: exps){
                 if(validExp.get(exp)==Boolean.FALSE) continue;
                 //remove the related common expression cause an assignment expression could be seen two times.
                 if (CheckForCommonExpression(exps, exp,validExp)) continue;
                 //continue with the remaining logic
                 String expType;
-                        try{
-                            expType=exp.calculateResolvedType().asReferenceType().getQualifiedName();
-                        }
-                        catch (Exception e){
-                            expType = "dummySymbol";
-                            System.out.println("NOT PARSING CLASS");
-                        }
+                try{
+                    expType=exp.calculateResolvedType().asReferenceType().getQualifiedName();
+                }
+                catch (Exception e){
+                    expType = "dummySymbol";
+                    System.out.println("NOT PARSING CLASS");
+                }
                 if(interfaceToLookup.contains(expType)) {
                     System.out.println(exp.toString());
                     if(exp.isAssignExpr()){
                         AssignExpr ae = (AssignExpr) exp;
                         Expression target = ae.getTarget();
                         System.out.println("IS ASSIGN EXPR: "+target);
-
-                        //Getting all REFERENCES to target
                         GetLinesUsage(lines, localCu, target);
-
                     }
-
                     //getting all types in processed CU
                     List<TypeDeclaration<?>> typeDeclarations = localCu.get().getTypes();
                     if(!lines.isEmpty()) {
@@ -149,7 +107,6 @@ public class ContractDiscovery {
                         InnestCode(lines, typeDeclarations);
                     }
                 }
-
             };
             if(reWriteClass==1){
                 try {
@@ -159,6 +116,43 @@ public class ContractDiscovery {
                 }
             }
         });
+    }
+
+    private static String SelectProjectsPath() {
+        JFileChooser f = new JFileChooser();
+        JFileChooser f2 = new JFileChooser();
+        FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter(
+                "xml files (*.xml)", "xml");
+
+        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        f.setDialogTitle("Open project FOLDER");
+        f.showSaveDialog(null);
+
+        path = f.getCurrentDirectory().toString();
+        System.out.println("SELECTED:"+path);
+
+        String lastPath = path.substring(path.lastIndexOf('/') + 1);
+        if(!lastPath.equals("java")) {
+            System.out.println("project path is wrong, make sure that your last folder is /java");
+            System.exit(1);
+        }
+
+        typeSolver = new JavaParserTypeSolver(new File(path));
+
+        f2.setDialogTitle("Open pom file");
+        f2.setFileFilter(xmlfilter);
+        f2.showOpenDialog(null);
+        f2.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        String pomPath = f2.getSelectedFile().getAbsolutePath();
+        System.out.println(pomPath);
+        return pomPath;
+    }
+
+    private static void AddImportDeclaration(Optional<CompilationUnit> localCu) {
+        localCu.get().addImport(new ImportDeclaration("dev.failsafe.Failsafe", false, false));
+        localCu.get().addImport(new ImportDeclaration("dev.failsafe.RetryPolicy", false, false));
+        localCu.get().addImport(new ImportDeclaration("java.time.temporal.ChronoUnit", false, false));
     }
 
     private static void reWriteClass(CompilationUnit cu) throws IOException {
@@ -275,52 +269,43 @@ public class ContractDiscovery {
             NodeList<Statement> statements= b.getStatements();
             Iterator<Statement> iter = statements.iterator();
             HashMap<Integer,Integer> alreadyVisited =new HashMap<Integer,Integer>();
-
             while(iter.hasNext()){
                 Statement s = iter.next();
                 if(s.isTryStmt()){
                     int beginLine = s.getRange().get().begin.line;
                     int endLine = s.getRange().get().end.line;
-                    System.out.println("\n\t\tFOUND TRY-CATCH at line:"+beginLine);
-                    //iter = lines.listIterator();
                     for (Integer line:lines){
-                        //Integer line = iter.next();
                         System.out.println("\t\t\tAnalyzing line:"+line);
-
                         boolean lineIsInStatement = ((endLine >= line) && (line >= beginLine));
                         if( lineIsInStatement ) {
                             System.out.println("\t\t\t\tINSERTING POLICY");
-                            //removing line. It is already placed AND mark statemant as visited
                             linesToRemove.add(line);
-                            //prendo blocco e statement
                             BlockStmt bs = s.findFirst(BlockStmt.class).get();
-
                             if(!alreadyVisited.containsKey(bs.getRange().get().begin.line)) {
-                                bs.addStatement(parseStatement("RetryPolicy<Object> retryPolicy = RetryPolicy.builder()\n" +
-                                        "    .handle(Exception.class)\n" +
-                                        "    .withBackoff(1, 30, ChronoUnit.SECONDS)\n" +
-                                        "    .withMaxRetries(3)\n" +
-                                        "    .onRetriesExceeded(e -> System.out.println(\"Failed to connect. Max retries exceeded.\"))\n" +
-                                        "    .build();"));
-                                alreadyVisited.put(bs.getRange().get().begin.line, bs.getRange().get().end.line);
+                                AddRetryPolicy(alreadyVisited, bs);
                             }
-
                             bs.findAll(Statement.class).forEach(st -> {
                                 if(st.getRange().get().begin.line == line){
                                     AddRetryCode(bs, st);
                                 }
                             });
-                        }
-                        else System.out.println("\t\t\t\tskipping...");
+                        } else System.out.println("\t\t\t\tskipping...");
                     }
-
-
                 } else System.out.println("\t\tAnother STMT ");
             };
             PrintAllStatements(statements);
-
         });
         return linesToRemove;
+    }
+
+    private static void AddRetryPolicy(HashMap<Integer, Integer> alreadyVisited, BlockStmt bs) {
+        bs.addStatement(parseStatement("RetryPolicy<Object> retryPolicy = RetryPolicy.builder()\n" +
+                "    .handle(Exception.class)\n" +
+                "    .withBackoff(1, 30, ChronoUnit.SECONDS)\n" +
+                "    .withMaxRetries(3)\n" +
+                "    .onRetriesExceeded(e -> System.out.println(\"Failed to connect. Max retries exceeded.\"))\n" +
+                "    .build();"));
+        alreadyVisited.put(bs.getRange().get().begin.line, bs.getRange().get().end.line);
     }
 
     private static void AddRetryCode(BlockStmt bs, Statement st) {
